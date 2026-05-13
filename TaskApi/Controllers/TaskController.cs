2e039624d82377cc651f1c77d47bdc4c207e6c2f
ZabrodinSA <81227269+ZabrodinSA@users.noreply.github.com>
2026-05-13 18:56:50 +0300

    Task API with CRUD operations and model definitions was added.

---
+++ TaskApi/Controllers/TaskController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TaskApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TasksController : Controller
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Hello, World!");
+    }
+    
+    [HttpPost]
+    public IActionResult Post([FromBody] PostTdo tdo)
+    {
+        return Ok("Task created! title: " + tdo.Title + ", priority: " + tdo.Priority );
+    }
+    
+    [HttpPut("{id}/complete")]
+    public IActionResult Complete(int id)
+    {
+        return Ok($"Task {id} marked as completed!");
+    }
+    
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        return Ok($"Task {id} deleted!");
+    }
+
+    public record PostTdo(string Title, int Priority);
+}
---
+++ TaskApi/Models/TaskModel.cs
@@ -0,0 +1,18 @@
+﻿namespace TaskApi.Models;
+
+public class TaskModel
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; } // не null, не пустой, максимум 200
+    public bool IsCompleted { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset? CompletedAt { get; set; } // когда завершили
+    public Priority Priority { get; set; } // Low, Medium, High
+}
+
+public enum Priority
+{
+    Low = 1,
+    Medium = 2,
+    High = 3
+}
