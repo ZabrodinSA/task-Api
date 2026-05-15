6d73ca337eae2382df3573093a9dd3fb0cefd51a
ZabrodinSA <81227269+ZabrodinSA@users.noreply.github.com>
2026-05-15 09:37:58 +0300

    Integration tests for TaskController was added.

---
+++ TaskApi.IntegrationTest/TaskControllerTest.cs
@@ -0,0 +1,77 @@
+﻿using System.Net.Http.Json;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using TaskApi.Models;
+
+namespace TaskApi.IntegrationTest;
+
+public class TaskControllerTest(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client = factory.CreateClient();
+    
+    [Fact]
+    public async Task GetTasks_ShouldReturn200()
+    {
+        // Arrange
+        var newTask = new
+        {
+            Title = "Test Task",
+            Priority = (int)Priority.Medium
+        };
+        
+        var rabbitConnectionFactory = new ConnectionFactory { HostName = "localhost" };
+        var rabbitConnection = await rabbitConnectionFactory.CreateConnectionAsync();
+        var rabbitChannel = await rabbitConnection.CreateChannelAsync();
+        
+        // Act
+        var createTaskResponse = await _client.PostAsJsonAsync("/tasks", newTask);
+
+        createTaskResponse.EnsureSuccessStatusCode();
+        var createdTask = await createTaskResponse.Content.ReadFromJsonAsync<TaskModel>();
+        Assert.NotNull(createdTask);
+
+        var completeTaskResponse = await _client.PutAsJsonAsync($"/tasks/{createdTask.Id}/complete", new { });
+        completeTaskResponse.EnsureSuccessStatusCode();
+
+        // Assert
+        var messageReceived = false;
+        var consumer = new CustomAsyncConsumer(rabbitChannel);
+        
+        consumer.OnMessageReceived += (body) =>
+        {
+            var message = Encoding.UTF8.GetString(body.ToArray());
+            var payload = JsonConvert.DeserializeObject<dynamic>(message);
+            if (payload != null && payload?.taskId == createdTask.Id.ToString())
+            {
+                messageReceived = true;
+            }
+        };
+        
+        await rabbitChannel.BasicConsumeAsync("task.completed.queue", false, consumer);
+        
+        await Task.Delay(1000); // Wait for the message to be consumed
+        Assert.True(messageReceived, "RabbitMQ did not receive the expected message.");
+    }
+    
+    public class CustomAsyncConsumer(IChannel channel) : AsyncDefaultBasicConsumer(channel)
+    {
+        private readonly IChannel _channel = channel;
+        
+        public Action<ReadOnlyMemory<byte>> OnMessageReceived { get; set; }
+
+        public override async Task HandleBasicDeliverAsync(
+            string consumerTag,
+            ulong deliveryTag,
+            bool redelivered,
+            string exchange,
+            string routingKey,
+            IReadOnlyBasicProperties properties,
+            ReadOnlyMemory<byte> body,
+            CancellationToken cancellationToken = default)
+        {
+            OnMessageReceived(body);
+        }
+    }
+}
--- TaskApi/Program.cs
+++ TaskApi/Program.cs
@@ -47,3 +47,5 @@ app.MapControllers();
 
 app.Run();
 
+public partial class Program { }
+
